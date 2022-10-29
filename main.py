b22eb83600c5231a06c1aa5e6c5a1d9462a168fd
dhanushka365 <technohub10000@gmail.com>
2022-10-29 10:09:53 +0530

    commit 2

--- main.py
+++ main.py
@@ -46,6 +46,46 @@ def drow_the_lines(img,lines):
     return img
 #####################################################################################################################
 
+def display_lines(image, lines):
+    image=np.copy(image)
+    line_image = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
+    #line_image =np.zeros_like(image)
+    if lines is not None:
+        for x1,y1,x2,y2 in lines:
+            cv2.line(line_image, (x1,y1),(x2,y2),(0,255,0),10)
+    return line_image
+####################################################################################################################
+def make_coordinates(iamge ,line_paarameters):
+    slope ,intercept = line_paarameters
+    y1 = image.shape[0]
+    y2 = int(y1*(2/5))
+    x1 = int((y1 -intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1,y1,x2,y2])
+#####################################################################################################################
+
+def average_slope_itercept(image ,lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 =line.reshape(4)
+        parameters =np.polyfit((x1,x2), (y1,y2),1)
+        #print(parameters)
+        slope =parameters[0]
+        intercept =parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope,intercept)) 
+    left_fit_average =np.average(left_fit,axis=0)
+    right_fit_average= np.average(right_fit, axis=0)
+    left_line = make_coordinates(image,left_fit_average)
+    right_line = make_coordinates(image , right_fit_average)
+    print(left_fit_average, 'left')
+    print(right_fit_average, 'right')
+    return np.array([left_line ,right_line])
+
+####################################################################################################################
 #gray_image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
 canny_image = cv2.Canny(gray_image, 50, 150)
 
@@ -53,11 +93,23 @@ cropped_image = region_of_interest(canny_image, np.array([region_of_interest_ver
 
 ######################################################################################################################
 
+
 lines =cv2.HoughLinesP(cropped_image, rho=10,theta=np.pi/180, threshold=100,lines=np.array([]), minLineLength=100, maxLineGap=2)
 
-image_with_lines =drow_the_lines(image, lines)
+#image_with_lines =drow_the_lines(image, lines)
+
+averaged_lines =average_slope_itercept(lane_image,lines)
+#image_with_lines =drow_the_lines(lane_image,averaged_lines)
+
+
+#######################################################
+line_image1 =display_lines(lane_image , averaged_lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image1,1,1)
+
+########################################################
 #sobel_image = cv2.Sobel(gray_image, 100,20)
 plt.imshow(canny_image)
-cv2.imshow("result", cropped_image)
-plt.imshow(image_with_lines)
+#cv2.imshow("result", cropped_image)
+cv2.imshow("result1",combo_image)
+#plt.imshow(image_with_lines)
 plt.show()
