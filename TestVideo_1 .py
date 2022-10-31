edea8ac500b80be7ea12235a197e33fc485829ab
dhanushka365 <technohub10000@gmail.com>
2022-10-31 23:10:08 +0530

    work commit

---
+++ TestVideo_1 .py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+import matplotlib.image as mpimg
+import glob
+#######################################################################################################################################
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+#######################################################################################################################################
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+#########################################################################################################################################
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray,(5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+########################################################################################################################################
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
+    return line_image
+########################################################################################################################################
+def region_of_interest(image):
+    height = image.shape[0]
+    #polygons = np.array([[(200, height), (1100, height), (550, 250)]])
+    polygons = np.array([[(0, image.shape[0]), (250, 290), (180, 320), (image.shape[1], image.shape[0])]])
+    #polygons = np.array([[(0, image.shape[0]),(0, image.shape[0]-100), (290, 320), (340, 320), (image.shape[1]-150, image.shape[0])]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+def mark_lanes(lane_image):
+    canny_image = canny(lane_image)
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(lane_image, lines)
+    line_image = display_lines(lane_image, averaged_lines)
+    combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+    return combo_image
+
+def createVideo():
+    img_array = []
+    for filename in glob.glob('Output/*.jpg'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        img_array.append(img)
+
+    out = cv2.VideoWriter('project1.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+    
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
+
+paths = glob.glob('TestVideo_1/*.bmp')
+
+for i,image_path in enumerate(paths):
+    image = mpimg.imread(image_path)
+    lane_image = np.copy(image)
+    result = mark_lanes(lane_image)
+    # plt.subplot(2,3,6)
+    # plt.imshow(result)
+    mpimg.imsave('Output_TestVideo_1/'+image_path[12:-4]+'_detected.jpg', result)
+
+createVideo()
--- main.py
+++ main.py
@@ -1,18 +1,31 @@
+#18_ENG_112
 #importing some useful packages
+from cv2 import line
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
 import math
+import os
+from os import listdir
 
+
+####################################################################################################
+image_folder = 'TestVideo_1'
+video_name = "test1.avi"
+images = [img for img in os.listdir(image_folder) if img.endswith(".bmp")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
 #########################################################################################################
-#reading in an image
-image = mpimg.imread('/home/uduwela/Project-Finding-Lane-Lines-on-the-Road-master/TestVideo_1/Right_22.bmp')
-lane_image = np.copy(image)
-#printing out some stats and plotting
-#print('This image is:', type(image), 'with dimensions:', image.shape)
-#plt.imshow(image)  
-#plt.show()
+
 ######################################################################################################################
 
 def grayscale(img):
@@ -20,15 +33,11 @@ def grayscale(img):
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # Or use BGR2GRAY if you read an image with cv2.imread()
     # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-def canny(img, low_threshold, high_threshold):
-    """Applies the Canny transform"""
-    return cv2.Canny(img, low_threshold, high_threshold)
-
+#############################################################################################################    
 def gaussian_blur(img, kernel_size):
     """Applies a Gaussian Noise kernel"""
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
-
+###############################################################################################################
 def region_of_interest(img, vertices):
     """
     Applies an image mask.
@@ -38,7 +47,7 @@ def region_of_interest(img, vertices):
     
     #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
     if len(img.shape) > 2:
-        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        channel_count = img.shape[3]  # i.e. 3 or 4 depending on your image
         ignore_mask_color = (255,) * channel_count
     else:
         ignore_mask_color = 255
@@ -50,7 +59,7 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
-
+#####################################################################################################################
 def draw_lines(img, lines, color=[255, 0, 0], thickness=3):
     # Make a copy of the original image.
     img = np.copy(img)
@@ -75,13 +84,6 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=3):
     # Return the modified image.
     return img
 
-####################################################################################################################
-#gray scale image conversion
-gray = grayscale(image)
-#####################################################################################################################
-#Gaussian blur
-kernel_size = 5
-blur_gray = gaussian_blur(gray,kernel_size)
 ###################################################################################################################
 # Return a thresholded image using sobel magnitude and angle
 def sobel_threshold(img, mag_thresh = (30,255),ang_thresh = (.8, 1.4)):
@@ -127,79 +129,240 @@ def show_example_sobel(img):
     print(saturation_sobel_img)
     return saturation_sobel_img
     
-###################################################################################################################
-edges = show_example_sobel(lane_image)
-#####################################################################################################################
-# This time we are defining a four sided polygon to mask
-imshape = image.shape
-height = image.shape[0]#get the hieght of image
-width = image.shape[1]#get the width of image
-region_of_interest_vertices = [
-    (0, height),
-    (0 , height/2 +50),
-    (width , height/2 +50),
-    (width, height)
-]
-################################################################################################################
-vertices = np.array([region_of_interest_vertices], dtype=np.int32)
-masked_edges = region_of_interest(edges, vertices)
-#plt.imshow(masked_edges)
+
+###########################################################################################################
+def make_coordinates(iamge ,line_paarameters):
+    slope ,intercept = line_paarameters
+    y1 = image.shape[0]
+    y2 = int(y1*(2.9/5))
+    x1 = int((y1 -intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1,y1,x2,y2])
+#############################################################################################################
+def average_slope_itercept(image ,lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 =line.reshape(4)
+        parameters =np.polyfit((x1,x2), (y1,y2),1)
+        #print(parameters)
+        slope =parameters[0]
+        intercept =parameters[1]
+        if (slope <= -0.5 and slope >= -1.5):
+            left_fit.append((slope, intercept))
+        elif(slope >= 0.5 and slope <= 1.5):
+            right_fit.append((slope,intercept)) 
+    left_fit_average =np.average(left_fit,axis=0)
+    right_fit_average= np.average(right_fit, axis=0)
+    left_line = make_coordinates(image,left_fit_average)
+    right_line = make_coordinates(image , right_fit_average)
+    print(left_fit_average, 'left')
+    print(right_fit_average, 'right')
+    return np.array([left_line ,right_line])
 
 ##############################################################################################################
-# Define the Hough transform parameters
-# Make a blank the same size as our image to draw on
-rho = 10 # distance resolution in pixels of the Hough grid
-theta = np.pi/180 # angular resolution in radians of the Hough grid
-threshold = 70    # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 70 #minimum number of pixels making up a line
-max_line_gap = 1 # maximum gap in pixels between connectable line segments
+def display_lines(image, lines):
+    image=np.copy(image)
+    line_image = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
+    #line_image =np.zeros_like(image)
+    if lines is not None:
+        for x1,y1,x2,y2 in lines:
+            cv2.line(line_image, (x1,y1),(x2,y2),(255,0,0),3)
+    return line_image
 
-# Run Hough on edge detected image
-# Output "lines" is an array containing endpoints of detected line segments
-lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
-#print(lines)
-left_line_x = []
-left_line_y = []
-right_line_x = []
-right_line_y = []
+###############################################################################################################
 
-for line in lines:
-    for x1, y1, x2, y2 in line:
-        slope = (y2 - y1) / (x2 - x1)
-        if (math.fabs(slope) < 0.5 and math.fabs(slope) > -0.5):
-            continue
-        if (slope <= -0.5 and slope >= -1.5):
-            left_line_x.extend([x1, x2])
-            left_line_y.extend([y1, y2])
-        elif(slope >= 0.5 and slope <= 1.5):
-            right_line_x.extend([x1, x2])
-            right_line_y.extend([y1, y2])
+#############################################################################################################
+def average_slope_itercept1(image ,lines):
+    left_line_x = []
+    left_line_y = []
+    right_line_x = []
+    right_line_y = []
+
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            slope = (y2 - y1) / (x2 - x1)
+            if (math.fabs(slope) < 0.5 and math.fabs(slope) > -0.5):
+                continue
+            if (slope <= -0.5 and slope >= -1.5):
+                left_line_x.extend([x1, x2])
+                left_line_y.extend([y1, y2])
+            elif(slope >= 0.5 and slope <= 1.5):
+                right_line_x.extend([x1, x2])
+                right_line_y.extend([y1, y2])
             
-min_y = int(image.shape[0] * (2 / 5))
-max_y = int(image.shape[0])
+    min_y = int(image.shape[0] * (2 / 5))
+    max_y = int(image.shape[0])
 
-poly_left = np.poly1d(np.polyfit(left_line_y, left_line_x, deg=1))
+    poly_left = np.poly1d(np.polyfit(left_line_y, left_line_x, deg=1))
 
-left_x_start = int(poly_left(max_y))
-left_x_end = int(poly_left(min_y))
+    left_x_start = int(poly_left(max_y))
+    left_x_end = int(poly_left(min_y))
 
-poly_right = np.poly1d(np.polyfit(right_line_y, right_line_x, deg=1))
+    poly_right = np.poly1d(np.polyfit(right_line_y, right_line_x, deg=1))
 
-right_x_start = int(poly_right(max_y))
-right_x_end = int(poly_right(min_y))
+    right_x_start = int(poly_right(max_y))
+    right_x_end = int(poly_right(min_y))
+    #print(poly_left)
+    x1= left_x_start
+    y1= max_y
+    x2= left_x_end
+    y2= min_y
+    x3 = right_x_start
+    y3 = max_y
+    x4=right_x_end
+    y4=min_y
+    u = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1))
+    #print(u)
+    x = x1 + u * (x2-x1)
+    y = y1 + u * (y2-y1)
+    #print(x,y)
+    line_image = draw_lines(
+            image,
+            [[
+                [left_x_start, max_y, left_x_end, min_y],
+                [right_x_start, max_y, right_x_end, min_y],
+            ]],
+            thickness=5,
+            )
+    circle_image =cv2.circle(line_image, (int(x),int(y)), 10,(255,255,0),3 )
+    final_image = cv2.addWeighted(line_image, 1, circle_image,0.1,1)     
+    plt.figure()
+    plt.imshow(final_image)
+    plt.show()       
+############################################################################################################
 
-
-line_image = draw_lines(
-        image,
-        [[
-            [left_x_start, max_y, left_x_end, min_y],
-            [right_x_start, max_y, right_x_end, min_y],
-        ]],
-        thickness=5,
-        )
-
-plt.figure()
-plt.imshow(line_image)
-plt.show()
 
 ################################################################################################################
+def process_image1(image):
+    image_path = "images"
+    #gray scale image conversion
+    gray = grayscale(image)
+
+    #Gaussian blur
+    kernel_size = 5
+    blur_gray = gaussian_blur(gray,kernel_size)
+
+    edges = show_example_sobel(lane_image)
+
+
+    # This time we are defining a four sided polygon to mask
+    imshape = image.shape
+    height = image.shape[0]#get the hieght of image
+    width = image.shape[1]#get the width of image
+    region_of_interest_vertices = [
+        (0, height),
+        (0 , height/2 +50),
+        (width , height/2 +50),
+        (width, height)
+    ]
+
+    vertices = np.array([region_of_interest_vertices], dtype=np.int32)
+    masked_edges = region_of_interest(edges, vertices)
+    #plt.imshow(masked_edges)
+
+    # Define the Hough transform parameters
+    # Make a blank the same size as our image to draw on
+    rho = 10 # distance resolution in pixels of the Hough grid
+    theta = np.pi/180 # angular resolution in radians of the Hough grid
+    threshold = 100    # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 40 #minimum number of pixels making up a line
+    max_line_gap = 1 # maximum gap in pixels between connectable line segments
+
+    # Run Hough on edge detected image
+    # Output "lines" is an array containing endpoints of detected line segments
+    lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+    #print(lines)
+
+
+    #average_slope_itercept(image, lines)
+    averaged_lines =average_slope_itercept(image,lines)
+    print(averaged_lines)
+    line_image1 =display_lines(image , averaged_lines)
+    x1 = averaged_lines[0][0]
+    y1 = averaged_lines[0][1]
+    x2 = averaged_lines[0][2]
+    y2 = averaged_lines[0][3]
+    x3 = averaged_lines[1][0]
+    y3 = averaged_lines[1][1]
+    x4 = averaged_lines[1][2]
+    y4 = averaged_lines[1][3]
+
+    u = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1))
+    print(u)
+    x = x1 + u * (x2-x1)
+    y = y1 + u * (y2-y1)
+    print(x,y)
+    combo_image = cv2.addWeighted(image, 0.8, line_image1,1,1)
+
+    cv2.circle(combo_image, (int(x),int(y)), 10,(255,255,0),3 )
+    cv2.imshow("line result", combo_image)
+    #cv2.imshow('result',cropped_image)
+    cv2.waitKey(0) #display result window infinetly untill we press anything from keyboard
+
+##############################################################################################################################
+def process_image(image):
+    image_path = "images"
+    #gray scale image conversion
+    gray = grayscale(image)
+
+    #Gaussian blur
+    kernel_size = 5
+    blur_gray = gaussian_blur(gray,kernel_size)
+
+    edges = show_example_sobel(lane_image)
+
+
+    # This time we are defining a four sided polygon to mask
+    imshape = image.shape
+    height = image.shape[0]#get the hieght of image
+    width = image.shape[1]#get the width of image
+    region_of_interest_vertices = [
+        (0, height),
+        (0 , height/2 +50),
+        (width , height/2 +50),
+        (width, height)
+    ]
+
+    vertices = np.array([region_of_interest_vertices], dtype=np.int32)
+    masked_edges = region_of_interest(edges, vertices)
+    #plt.imshow(masked_edges)
+
+    # Define the Hough transform parameters
+    # Make a blank the same size as our image to draw on
+    rho = 1 # distance resolution in pixels of the Hough grid
+    theta = np.pi/180 # angular resolution in radians of the Hough grid
+    threshold = 100    # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 40 #minimum number of pixels making up a line
+    max_line_gap = 1 # maximum gap in pixels between connectable line segments
+
+    # Run Hough on edge detected image
+    # Output "lines" is an array containing endpoints of detected line segments
+    lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+    #print(lines)
+
+
+    average_slope_itercept1(image, lines)
+
+##############################################################################################################################
+# get the path or directory
+folder_dir = "/home/uduwela/co4204_lab_2_road_lane_detection-dhanushka365/TestVideo_1"
+for images in os.listdir(folder_dir):
+ 
+    # check if the image ends with png or jpg or jpeg
+    if (images.endswith(".bmp")):
+        # display
+        a= folder_dir +"/"+ images
+        #reading in an image
+        image = mpimg.imread('/home/uduwela/co4204_lab_2_road_lane_detection-dhanushka365/TestVideo_1/Right_18.bmp')
+        lane_image = np.copy(image)
+        #printing out some stats and plotting
+        #print('This image is:', type(image), 'with dimensions:', image.shape)
+        #plt.imshow(image)  
+        #plt.show()
+        process_image(lane_image)
+        #process_image1(lane_image)
+
+
+
+        # Try to work with sobel operator but wasn't able to to identify the corrrect treshhold values .therefore the function gives a weeka edge image at last.
